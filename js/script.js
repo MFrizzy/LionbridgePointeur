91aefc475b0dcdbc9071149a1d6849888be79e32
Unknown <at.frizzy@gmail.com>
2018-02-09 09:25:06 +0100

    style + changement affichage

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-//heure = document.getElementById("heure");
-h = new Date();
-console.log(h);
-console.log("Mois : " + h.getMonth() + 1);
-console.log("Jour : " + h.getDay());
-console.log("minutes : " + h.getMinutes());
-console.log("heure : " + h.getHours());
-
-//date = h.toLocaleDateString() + " " + h.getHours() + ":" + h.getMinutes()
-console.log(h.getTime())
-//heure.innerHTML = date;
-
 class Horaire {
     constructor(date) {
         this.objet = date.getTime()
@@ -19,7 +7,9 @@ class Horaire {
     }
 
     getHeure() {
-        return this.heure + ":" + this.minutes
+        var heures = this.heure<10?"0"+this.heure:this.heure;
+        var minute = this.minutes<10?"0"+this.minutes:this.minutes
+        return heures + ":" + minute
     }
 }
 
@@ -33,10 +23,6 @@ class Session {
         this.fin = new Horaire(date);
     }
 
-    resume() {
-        this.fin = false;
-    }
-
     isFinished(date) {
         return this.fin != false;
     }
@@ -48,8 +34,13 @@ class Session {
     getTWInteractif() {
         var tempsMs = Date.now() - this.debut.objet
         var tempsS = tempsMs/1000;
-        return Math.round(tempsS/3600,2);
+        return precisionRound(tempsS/3600,2);
     }
+}
+
+function precisionRound(number, precision) {
+    var factor = Math.pow(10, precision);
+    return Math.round(number * factor) / factor;
 }
 
 var currentSession = null;
@@ -58,12 +49,13 @@ function nouveau() {
     if (currentSession == null) {
         currentSession = new Session(new Date());
         document.getElementById("new").disabled = true;
-        document.getElementById("resume").disabled = true;
         document.getElementById("finish").disabled = false;
         document.getElementById("DD").innerText = currentSession.debut.getHeure();
+        document.getElementById("add").disabled=false;
+        document.getElementById("TW").innerText="0.00";
         setInterval(function () {
             document.getElementById("TW").innerText = currentSession.getTWInteractif();
-        }, 1000*60)
+        }, 1000*30)
     }
 }
 
@@ -71,23 +63,20 @@ function finish() {
     if (currentSession != null) {
         currentSession.end(new Date());
         document.getElementById("new").disabled = false;
-        document.getElementById("resume").disabled = false;
         document.getElementById("finish").disabled = true;
         document.getElementById("DD").innerText = "";
+        document.getElementById("add").disabled=true;
+        document.getElementById("remove").disabled=true;
+        document.getElementById("compt").innerText="0";
+        // Faire un recap
         currentSession.save()
+        currentSession=null;
     }
 }
 
-function resume() {
-    if (currentSession != null) {
-        currentSession.resume();
-        document.getElementById("new").disabled = true;
-        document.getElementById("resume").disabled = true;
-        document.getElementById("finish").disabled = false;
-    }
-}
 
 function add() {
+    // ajouter à la current session
     var compteur = document.getElementById("compt");
     compteur.innerText = Number(compteur.innerText) + 1;
     if (Number(compteur.innerText) > 0)
