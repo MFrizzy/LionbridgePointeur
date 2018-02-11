9cd848436e6a01d7a4c17e13af2d02a71ade0099
Unknown <at.frizzy@gmail.com>
2018-02-11 23:40:02 +0100

    rectifs perfs

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ class Horaire {
     }
 
     getHeure() {
-        var heures = this.heure<10?"0"+this.heure:this.heure;
-        var minute = this.minutes<10?"0"+this.minutes:this.minutes
+        var heures = this.heure < 10 ? "0" + this.heure : this.heure;
+        var minute = this.minutes < 10 ? "0" + this.minutes : this.minutes
         return heures + ":" + minute
     }
 }
@@ -19,10 +19,11 @@ class Session {
         this.fin = false;
     }
 
-    end(date,nbTaches) {
+    end(date, nbTaches) {
         this.fin = new Horaire(date);
         this.nbTaches = nbTaches;
     }
+
     /**
      * Session.isFinished() should be true
      */
@@ -30,9 +31,9 @@ class Session {
         var li = "<li class=\"mdc-list-item\">\n" +
             "                    <i class=\"mdc-list-item__graphic material-icons\" aria-hidden=\"true\">keyboard_arrow_right</i>\n" +
             "                    <span class=\"mdc-list-item__text\">\n" +
-            "                        "+this.debut.getHeure()+" - "+this.fin.getHeure()+" : "+this.getFinalTW()+" heures\n" +
+            "                        " + this.debut.getHeure() + " - " + this.fin.getHeure() + " : " + this.getFinalTW() + " heures\n" +
             "                        <span class=\"mdc-list-item__secondary-text\">\n" +
-            "                            "+this.nbTaches+" Tâches\n" +
+            "                            " + this.nbTaches + " Tâches\n" +
             "                        </span>\n" +
             "                    </span>\n" +
             "                </li>"
@@ -48,8 +49,8 @@ class Session {
      */
     getFinalTW() {
         var tempsMs = this.fin.objet - this.debut.objet
-        var tempsS = tempsMs/1000;
-        return precisionRound(tempsS/3600,2);
+        var tempsS = tempsMs / 1000;
+        return precisionRound(tempsS / 3600, 2);
     }
 
     save() {
@@ -58,8 +59,8 @@ class Session {
 
     getTWInteractif() {
         var tempsMs = Date.now() - this.debut.objet
-        var tempsS = tempsMs/1000;
-        return precisionRound(tempsS/3600,2);
+        var tempsS = tempsMs / 1000;
+        return precisionRound(tempsS / 3600, 2);
     }
 }
 
@@ -76,35 +77,37 @@ function nouveau() {
         document.getElementById("new").disabled = true;
         document.getElementById("finish").disabled = false;
         document.getElementById("DD").innerText = currentSession.debut.getHeure();
-        document.getElementById("add").disabled=false;
-        document.getElementById("TW").innerText="0.00";
+        document.getElementById("add").disabled = false;
+        document.getElementById("TW").innerText = "0.00";
         setInterval(function () {
-            document.getElementById("PF").innerText = Number(document.getElementById("compt").innerText)/(30*currentSession.getTWInteractif())
+            document.getElementById("PF").innerText = precisionRound(Number(document.getElementById("compt").innerText) / (30 * currentSession.getTWInteractif()),2)
             document.getElementById("TW").innerText = currentSession.getTWInteractif();
-        }, 1000*30)
+        }, 1000 * 30)
     }
 }
 
 function finish() {
     if (currentSession != null) {
-        currentSession.end(new Date(),document.getElementById("compt").innerText);
+        currentSession.end(new Date(), document.getElementById("compt").innerText);
         document.getElementById("new").disabled = false;
         document.getElementById("finish").disabled = true;
         document.getElementById("DD").innerText = "";
-        document.getElementById("add").disabled=true;
-        document.getElementById("remove").disabled=true;
-        document.getElementById("histoj").innerHTML+=currentSession.generateLi();
-        document.getElementById("compt").innerText="0";
+        document.getElementById("add").disabled = true;
+        document.getElementById("remove").disabled = true;
+        document.getElementById("histoj").innerHTML += currentSession.generateLi();
+        document.getElementById("compt").innerText = "0";
+        document.getElementById("PF").innerText = "";
+        document.getElementById("TW").innerText = "";
         currentSession.save()
-        currentSession=null;
+        currentSession = null;
     }
 }
 
 
 function add() {
-    // ajouter à la current session
     var compteur = document.getElementById("compt");
     compteur.innerText = Number(compteur.innerText) + 1;
+    majPerf();
     if (Number(compteur.innerText) > 0)
         document.getElementById("remove").disabled = false;
 }
@@ -117,6 +120,10 @@ function remove() {
     if (Number(compteur.innerText) <= 0) {
         document.getElementById("remove").disabled = true;
     }
+    majPerf();
 }
 
+function majPerf() {
+    document.getElementById("PF").innerText = precisionRound(Number(document.getElementById("compt").innerText) / (30 * currentSession.getTWInteractif()))
+}
 
